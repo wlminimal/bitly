238ec6accf64a608b5a4093b2f0fe7f2f81c66ff
Taehwan Kim <wlminimal@gmail.com>
2018-08-29 09:40:52 -0700

    edit mix.exs for umbrella app

--- mix.exs
+++ mix.exs
@@ -5,6 +5,10 @@ defmodule Bitly.MixProject do
     [
       app: :bitly,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
